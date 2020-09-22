143616c0d61bc17726bdf220e611b5c7d2aca75d
tubleronchik <m.bystramovich@gmail.com>
2020-09-22 13:56:11 +0300

    update deleted API

--- server/server.js
+++ server/server.js
@@ -121,7 +121,7 @@ server.delete('/api/v1/users/:userId', (req, res) => {
 
 server.delete('/api/v1/users/', (req,res) => {
   unlink(`${__dirname}/users.json`)
-  res.status(200)
+  res.json({status: 'success'})
 })
 
 

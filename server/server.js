4c4b4e48101e102d8141389cf69dd9ffe8de8304
tubleronchik <m.bystramovich@gmail.com>
2020-09-24 23:03:15 +0300

    update patch api

--- server/server.js
+++ server/server.js
@@ -96,10 +96,7 @@ server.patch('/api/v1/users/:userId', (req, res) => {
       const newArr = users.reduce((acc, user) => {
         return user.id === +userId ? [acc, {id: +userId, ...body}] : [...acc, user]
       })
-      return newArr
-    })
-    .then(data => {
-      addToFile('/users.json', data)
+      addToFile('/users.json', newArr)
       res.json({status: 'success', userId})
     })
 })
